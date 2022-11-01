d1b0baf66b6dc7202e042bc9226b3cb7f03af05e
M4RT4Z4K <116658532+M4RT4Z4K@users.noreply.github.com>
2022-11-01 17:24:34 +0100

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,24 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+  public:
+	Wektor2D(int a = 3, int b = 4) {
+		x = a;
+		y = b;
+		std::cout << "stworzylem wektor[" << x << "," << y << "]\n";
+	}
+	~Wektor2D() {
+		std::cout << "zniszczylem wektor ["<<x<<","<<y<<"]\n";
+	}
+	void print()
+	{
+		std::cout << " x= " << x << '\n' << " y= " << y << '\n';
+	}
+	void setX(int a) { x = a; }
+	void setY(int b) { y=b; }
+	int getX() { return x; }
+	int getY() { return y; }
+
+private:
+	int x;
+	int y;
 };

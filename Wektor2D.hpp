3d5ca2074a2e6ce6be028e11fee466a972c41f1b
M4RT4Z4K <116658532+M4RT4Z4K@users.noreply.github.com>
2022-11-01 17:28:32 +0100

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,7 +1,7 @@
 class Wektor2D
 {
   public:
-	Wektor2D(int a = 3, int b = 4) {
+	Wektor2D(double a = 0, double b = 0) {
 		x = a;
 		y = b;
 		std::cout << "stworzylem wektor[" << x << "," << y << "]\n";
@@ -13,12 +13,23 @@ class Wektor2D
 	{
 		std::cout << " x= " << x << '\n' << " y= " << y << '\n';
 	}
-	void setX(int a) { x = a; }
-	void setY(int b) { y=b; }
-	int getX() { return x; }
-	int getY() { return y; }
+	void setX(double a) { x = a; }
+	void setY(double b) { y=b; }
+	double getX() { return x; }
+	double getY() { return y; }
 
 private:
-	int x;
-	int y;
+	double x;
+	double y;
 };
+
+Wektor2D operator+(Wektor2D u, Wektor2D v) {
+	double x = u.getX()+ v.getX();
+	double y = u.getY() + v.getY();
+	return Wektor2D(x, y);
+}
+
+double operator*(Wektor2D u, Wektor2D v) {
+	double il=u.getX() * v.getX()+ u.getY() * v.getY();
+	return il;
+}
